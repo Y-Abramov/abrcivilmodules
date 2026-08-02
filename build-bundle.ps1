ebb1b76fd055b0990073e465ccb9c38a21379d3a
Y-Abramov <yarlif22@gmail.com>
2026-08-02 23:10:22 +0500

    build: упаковка AbrCivilModules в .bundle

---
+++ build-bundle.ps1
@@ -0,0 +1,4 @@
+﻿$base = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
+. (Join-Path $base "..\Shared\BuildBundle.Common.ps1")
+
+Build-AbrBundle -Base $base -BundleName "AbrCivilModules" -AssemblyName "Abr.Civil.Modules"
